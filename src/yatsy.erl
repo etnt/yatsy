96451772561878f2558af3c199de690eee4eef65
torbjorn.tornkvist <torbjorn.tornkvist@a6d9eac6-8a1c-0410-8c09-6b58bb7c4570>
2006-09-04 21:11:22 +0000

    Added some CSS effects to the yatsy.yaws page.

--- src/yatsy.erl
+++ src/yatsy.erl
@@ -6,7 +6,7 @@
 
 -export([start/0, start_link/0, start/1, start_link/1,
 	 run/0, clean/0, clean_and_run/0,
-	 top/1, app/2, suite/3
+	 top/1, app/2, suite/3, tc/4
 	 ]).
 
 -export([test/0]).
@@ -40,6 +40,9 @@ clean() ->
 clean_and_run() ->
    yatsy_ts:clean_and_run().
 
+%%%
+%%% Report Generating functions
+%%%
 top(Url) ->
     yatsy_rg:top(Url).
 
@@ -48,5 +51,8 @@ app(Url, App) ->
 
 suite(Url, App, Suite) ->
     yatsy_rg:suite(Url, App, Suite).
+
+tc(Url, App, Suite, Tc) ->
+    yatsy_rg:tc(Url, App, Suite, Tc).
 
 
