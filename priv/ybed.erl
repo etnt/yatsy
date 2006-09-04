3c526c499752cbafa4ce46ecd4b0991422df15eb
torbjorn.tornkvist <torbjorn.tornkvist@a6d9eac6-8a1c-0410-8c09-6b58bb7c4570>
2006-09-04 14:35:16 +0000

    Almost got the yatsy.yaws page working.

---
+++ priv/ybed.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% File    : ybed.erl
+%%% Descr.  : Small embedded yaws example
+%%%
+%%%           Make sure to change the docroot below to suit 
+%%%           your situation.    
+%%%
+%%% Compile: erlc -I <path-to-Yaws-include-dir> -o ../ebin ybed.erl
+%%%
+%%%-------------------------------------------------------------------
+-module(ybed).
+-export([start/0]).
+
+-define(DOCROOT, "/home/tobbe/yatsy/priv/docroot").
+
+-include("yaws.hrl").
+
+start() ->
+    setup_embedded_yaws(),
+    application:start(yaws),
+    GC = yaws_config:make_default_gconf(false, ""),
+    SC = #sconf{port = 8888,
+		servername = "localhost",
+		listen = {0,0,0,0},
+		docroot = ?DOCROOT},
+    yaws_api:setconf(GC, [[SC]]).
+
+
+setup_embedded_yaws() ->
+    ok = application:load(yaws),
+    ok = application:set_env(yaws, embedded, true).
+
+
