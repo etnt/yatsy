466c202709775727e06544cc876f75beb0462066
torbjorn.tornkvist <torbjorn.tornkvist@a6d9eac6-8a1c-0410-8c09-6b58bb7c4570>
2006-09-05 08:22:03 +0000

    Added the yatsy:quick/0 function that starts everything (including Yaws).

--- src/yatsy.erl
+++ src/yatsy.erl
@@ -6,16 +6,41 @@
 
 -export([start/0, start_link/0, start/1, start_link/1,
 	 run/0, clean/0, clean_and_run/0,
-	 top/1, app/2, suite/3, tc/4
+	 top/1, app/2, suite/3, tc/4,
+	 start_yaws/0, start_yaws/4,
+	 quick/0
 	 ]).
 
 -export([test/0]).
 
 test() ->
+    start_yaws(),
     yatsy_ts:test(),
     yatsy_rg:start(),
     yatsy_ts:run().
 
+
+%%%
+%%% Start Yaws in embedded mode.
+%%%
+start_yaws() ->
+    start_yaws(yatsy_ts:yaws_docroot(),
+	       yatsy_ts:yaws_host(),
+	       yatsy_ts:yaws_port(),
+	       yatsy_ts:yaws_listen()).
+
+start_yaws(DocRoot, Host, Port, Listen) ->
+    yaws:start_embedded(DocRoot, Host, Port, Listen).
+
+    
+%%%
+%%% Yatsy Test Server interface
+%%%
+quick() ->
+    start_yaws(),
+    yatsy_ts:start(),
+    yatsy_rg:start(),
+    yatsy_ts:run().
 
 start() ->
     start([]).
